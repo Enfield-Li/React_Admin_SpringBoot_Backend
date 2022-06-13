2d47675b642ef2a89a22e4ab786ff2c6d08ba523
Enfield-Li <1076462153@qq.com>
2022-06-13 09:24:14 +0800

    Rename

--- server/src/main/java/com/example/demo/config/AppConfig.java
+++ server/src/main/java/com/example/demo/config/AppConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 // import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class MyConfig {
+public class AppConfig {
 
   // @Bean
   // PasswordEncoder passwordEncoder() {
